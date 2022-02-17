1cd1a81f3e42d5f27a1d340f4308a3bad9ed704e
adeankush3 <adeankush3@gmail.com>
2022-02-17 13:22:25 +0530

    [Ankush]Balance Parentheses Program Complete

---
+++ SimpleBalancedParentheses/SimpleBalancedParentheses/StackClass.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBalancedParentheses
+{
+    public class StackClass
+    {
+        public Node top;
+
+        public StackClass()
+        {
+            this.top = null;
+        }
+
+        public void Push(int data)
+        {
+            Node node = new Node(data);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+        }
+
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                System.Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+
+        public void Peek()
+        {
+            if (this.top == null)
+            {
+                System.Console.WriteLine("stack is empty");
+                return;
+            }
+        }
+
+        public void Pop()
+        {
+            if (this.top == null)
+            {
+                System.Console.WriteLine("Stack is empty");
+                return;
+            }
+            this.top = this.top.next;
+        }
+
+        public int IsEmpty()
+        {
+            while (this.top != null)
+            {
+                Peek();
+                Pop();
+            }
+            return 0;
+        }
+
+    }
+}
